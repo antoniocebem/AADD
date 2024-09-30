9df4428d6286433d7169626a3de509bdf2e0209c
antoniocebem <antonio@cebem.es>
2024-09-30 12:01:29 +0200

    modificación

--- EjemploFicherosBinarios/src/Principal.java
+++ EjemploFicherosBinarios/src/Principal.java
@@ -3,6 +3,16 @@ import java.io.*;
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		try {
+			leerFicheroDoubleCadenas(EscribirEnterosCadenas());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void escribirFicheroBinario()
+	{
 		DataOutputStream ds = null;
 		File miFichero = null;
 		try {
@@ -46,7 +56,6 @@ public class Principal {
 			e.printStackTrace();
 		}
 	}
-
 	public static void leerFicheroBinario(File ficheroALeer) throws IOException {
 		try {
 			FileInputStream fis=new FileInputStream(ficheroALeer);
@@ -55,11 +64,68 @@ public class Principal {
 				System.out.println(dis.readDouble());
 			}
 		} catch(EOFException eof) {
-			eof.printStackTrace();
+			System.out.println("Fin de fichero");
 		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public static void leerFicheroDoubleCadenas(File ficheroALeer) throws IOException {
+		try {
+			FileInputStream fis=new FileInputStream(ficheroALeer);
+			DataInputStream dis=new DataInputStream(fis);
+			while(true) {
+				System.out.println(dis.readDouble());
+				System.out.println(dis.readUTF());
+			}
+		} catch(EOFException eof) {
+			System.out.println("Fin de fichero");
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static File EscribirEnterosCadenas() {
+		DataOutputStream ds = null;
+		File miFichero = null;
+		try {
+			miFichero = GestionFicheros.seleccionFichero();
+			FileOutputStream fe = new FileOutputStream(miFichero, true);
+			ds = new DataOutputStream(fe);
+			// array de enteros
+
+			double[] array = { 5.25, 145.18, 2.37, 143.12, 147.10, 169.19, 47.65 };
+			String[] nombres = { "Beatriz", "Ana", "Maria", "Juan", "Pedro", "Teresa", "Marta" };
+			if(array.length==nombres.length) {
+				for(int i=0;i<nombres.length;i++) {
+					ds.writeDouble(array[i]);
+					ds.writeUTF(nombres[i]);
+				}
+				
+			}
+			
+
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("No se pudo abrir el fichero Enteros.txt");
+
+		} catch (IOException e) {
+			System.out.println("No se pudo escribir en el fichero Enteros.txt");
+		} finally {
+			try {
+				
+				ds.close();
+				return miFichero;
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return miFichero;
 	}
 }
